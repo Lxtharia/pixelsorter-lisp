8a5f77088d27ccc96fbebbc59ce14a654e5e02b2
Lxtharia <maud-lin@gmx.de>
2025-12-01 03:03:46 +0100

    Reorder stuff; Wonder why stuff doesn't work;

--- pixelsort.lisp
+++ pixelsort.lisp
@@ -39,39 +39,23 @@
 
 ;; ;;;;;;;;;;;;; ;;
 ;; SPAN-SELECTOR ;;
-(defclass span-selector () ()
-  (:DOCUMENTATION "Splits one path of pixels into one or more spans."))
-(defclass span-selector-plus () ()
-  (:DOCUMENTATION "Splits one path of pixels into one or more spans. But more extensible."))
-
-(defclass limit-selector (span-selector)
-  ((max-len :initarg :max)
-   (random-minimum :initarg :random-min)))
-
-(defclass threshold-selector (span-selector)
-  ;; TODO map criteria symbol to criteria fun in constructor
-  ((criteria-fun :initarg :criteria :accessor criteria-fun)))
-
-;; Should simply apply limit-selector :around threshold-selector
-(defclass limited-threshold-selector (threshold-selector limit-selector) ;; the order is important, because on-next-method should only call threshold-selector
-  ())
-
-(defclass masked-thresholder (threshold-selector limit-selector) ;; the order is important, because on-next-method should only call threshold-selector
-  ())
-
-;; Generics 
 (defgeneric select-spans (span-selector pixels path)
   (:DOCUMENTATION "Splits the list of coordinates into spans via different criteria and merges them with the values. Returns a list of (coord . value)"))
 
-(defmethod select-spans ((selector span-selector-plus) pixels path)
-  ;; Mark everything as good (2)
-  (loop for _ in path collect
-	2))
+; Two main classes
+(defclass span-selector () ()
+  (:DOCUMENTATION "Splits one path of pixels into one or more spans."))
+(defclass span-mark-selector () ()
+  (:DOCUMENTATION "Splits one path of pixels into one or more spans. But more extensible."))
 
-;; :DOCUMENTATION "Collects the spans using the list of markers.
-;; Black excludes a pixel and starts a new span.
-;; Gray includes the pixel but starts a new span.")
-(defmethod select-spans :around ((selector span-selector-plus) pixels path)
+;; Select Methods
+;; Advanced :around method for selecting spans
+;; The primary method only has to return a linear list of marks with the same length as the path.
+;; Meaning of marks:
+;; 0 (Black):    exclude pixel and start a new span
+;; 1 (Gray):     start a new span but include pixel
+;; else (White): collect pixel to the current-span
+(defmethod select-spans :around ((selector span-mark-selector) pixels path)
   (let ((spans '())
 	(current-span '()) ; TODO: There's gotta be a more functional way
 	; Marked path is a list of values 0,1,2
@@ -91,32 +75,67 @@
 		 (span-break (list (get-pixel-value (nth i path))))) ; Include the pixel in the next span
 		(t (push (get-pixel-value (nth i path)) current-span)))))
       (span-break)
-      ; (format t "Selected spans: ~a~%" spans)
+      (format t "Marks ~a~%" (first marked-path))
       spans)))
 
+;; TODO: this is called on limit-mark-selector instance
+;; All-selector
+(defmethod select-spans ((selector span-mark-selector) pixels path)
+  ;; Mark everything as good (3)
+  (loop for _ in path collect
+	3))
 
-; Collect everything
-; (defmethod select-spans ((selector span-selector) pixels path)
-;   (list (loop for (x . y) in path
-; 	      collect (cons (x . y) (aref pixels x y)))))
+;; Limit selector
+(defclass limit-mark-selector (span-mark-selector) ())
+(defmethod select-span ((selector limit-mark-selector) pixels path)
+  (loop for _ in path collect
+	0))
+	;  (loop for i from 0 below (length path)
+	; collect (if (= 0 (% i 50)) 1 0)))
+
+
+;;
+;; Using the simpler, manual span-selector.
+;; Methods need the actually implement the span collecting manually
+;; Yeah
+;;
+
+(defclass limit-selector (span-selector)
+  ((max-len :initarg :max)
+   (random-minimum :initarg :random-min)))
+
+(defclass threshold-selector (span-selector)
+  ;; TODO map criteria symbol to criteria fun in constructor
+  ((criteria-fun :initarg :criteria :accessor criteria-fun)))
+
+;; Should simply apply limit-selector :around threshold-selector
+(defclass limited-threshold-selector (threshold-selector limit-selector) ;; the order is important, because on-next-method should only call threshold-selector
+  ())
+
+(defclass masked-thresholder (threshold-selector limit-selector) ;; the order is important, because on-next-method should only call threshold-selector
+  ())
+
+(defmethod select-spans ((selector span-selector) pixels path)
+  (list (loop for (x . y) in path
+	      collect (cons (cons x y) (aref pixels x y)))))
 ;
-; (defmethod select-spans ((selector threshold-selector) pixels path)
-;   (let ((spans '())
-; 	(current-span '())) ; TODO: There's gotta be a more functional way
-;     (loop for (x . y) in path do
-;       (let ((px (aref pixels x y)))
-; 	(if (< (color-intensity px) 128)
-; 	    (push px current-span)
-; 	    (progn (push current-span spans)
-; 		   (setq current-span (list px))))))))
-;
-; (defmethod select-spans ((selector limit-selector) pixels path))
-;
-; (defmethod select-spans :around ((selector limit-selector) pixels path)
-;   (let ((spans (call-next-method))
-; 	(len 3))
-;     (loop for i from (length spans) downto 0 by len
-; 	  collect (subseq spans i (+ i len))))) ;; Remove the first element. Placeholder for splitting spans above the limits
+(defmethod select-spans ((selector threshold-selector) pixels path)
+  (let ((spans '())
+	(current-span '())) ; TODO: There's gotta be a more functional way
+    (loop for (x . y) in path do
+      (let ((px (aref pixels x y)))
+	(if (< (color-intensity px) 128)
+	    (push (cons (cons x y) px) current-span)
+	    (progn (push current-span spans)
+		   (setq current-span (list (cons (cons x y) px)))))))))
+
+(defmethod select-spans ((selector limit-selector) pixels path))
+
+(defmethod select-spans :around ((selector limit-selector) pixels path)
+  (let ((spans (call-next-method))
+	(len 3))
+    (loop for i from (length spans) downto 0 by len
+	  collect (subseq spans i (+ i len))))) ;; Remove the first element. Placeholder for splitting spans above the limits
 
 ;;;;;;;;;;;;;;; ;;
 ;; SORTING-ALGO ;;
@@ -183,14 +202,24 @@
 		 (call-next-method)) ; This simply calls threshold first.
 
 ; The magic that gets applied to everything that is or inherits limit-selector
-(defmethod test :around ((selector limit-selector))
-  (print "Cutting the result of next-method:")
-  (subseq (call-next-method) 0 5))
+(defmethod test :around ((selector span-mark-selector))
+  (print "Around spanmark>")
+  (format t "Around: ~a~%" (call-next-method))
+  (print "<Around spanmark"))
+
+(defmethod test ((selector span-mark-selector))
+  (print "Primary1")
+  (list 1 1 1))
+
+(defmethod test ((selector limit-mark-selector))
+  (print "Primary2")
+  (list 2 2 2))
 
 ; (test (make-instance 'threshold-selector ))
 ; (test (make-instance 'limit-selector))
 ; (test (make-instance 'limited-threshold-selector ))
-; (test (make-instance 'span-selector-plus))
+(test (make-instance 'span-mark-selector))
+(test (make-instance 'limit-mark-selector))
 
 
 ;;; Main code ;;;
@@ -200,7 +229,7 @@
 
 (let* ((sorted (pixelsort *image*
 			  (make-instance 'line-path)
-			  (make-instance 'span-selector-plus)
+			  (make-instance 'limit-mark-selector)
 			  (make-instance 'sorting-algo :sort-by 'hue))))
   (imago:write-image sorted "out.jpg"))
 
